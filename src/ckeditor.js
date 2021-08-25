f25614e3217f9e7d988927fad50e943eb16f3606
tigerbank <bank_hpk@hotmail.com>
2021-08-25 11:55:02 +0700

    Added button

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -88,7 +88,28 @@ ClassicEditor.defaultConfig = {
             { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
             { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
             { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
-            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+            {
+                model: 'blueButton',
+                view: {
+                    name: 'div',
+                    classes: 'ck-button button-blue'
+                },
+                title: 'Blue Button',
+                class: 'ck-button button-blue',
+                converterPriority: 'high'
+            },
+            {
+                model: 'orangeButton',
+                view: {
+                    name: 'div',
+                    classes: 'ck-button button-orange'
+                },
+                title: 'Orange Button',
+                class: 'ck-button button-orange',
+                converterPriority: 'high'
+            }
+           
         ]
     },
     fontSize: {
